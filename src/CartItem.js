cfd81cfd8f7583d27e444e06db2604b4b722b61a
@nain333 <hnain70@gmail.com>
2023-04-05 02:16:39 +0530

    Add Props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,59 +1,98 @@
-import React from 'react';
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            price:999,
-            tittle:'Phone',
-            qty:1,
-            img:''
+import React from "react";
+class CartItem extends React.Component {
+    // testing () {
+    //   const promise = new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //       resolve('done');
+    //     }, 5000);
+    //   })
+  
+    //   promise.then(() => {
+    //     // setState acts like a synchronus call
+    //     this.setState({ qty: this.state.qty + 10 });
+  
+    //     this.setState({ qty: this.state.qty + 10 });
+  
+    //     this.setState({ qty: this.state.qty + 10 });
+  
+    //     console.log('state', this.state);
+    //   });
+    // }
+    increaseQuantity = () => {
+      // this.state.qty += 1;
+      // console.log('this', this.state);
+      // setState form 1
+      // this.setState({
+      //   qty: this.state.qty + 1
+      // }, () => {});
+  
+      // setState form 2 - if prevState required use this
+      this.setState((prevState) => {
+        return {
+          qty: prevState.qty + 1
         }
-        
+      });
     }
-    increaseQuantity=()=>{
-        console.log('this.state:',this.state)
+  
+    decreaseQuantity = () => {
+      const { qty } = this.state;
+  
+      if (qty === 0) {
+        return;
+      }
+      // setState form 2 - if prevState required use this
+      this.setState((prevState) => {
+        return {
+          qty: prevState.qty - 1
+        }
+      });
     }
-    render(){
-        const{price,tittle,qty}=this.state;
-        return(
-            <div className='cart-item'>
-                <div className='left-block'>
-                    <img style={styles.image}/>
-                </div>
-                <div className='right-block'>
-                <div style={{fontSize:25}}>{tittle}</div>
-                <div style={{color:'#777'}}>Rs: {price}</div>
-                <div style={{color:'#777'}}>Qty: {qty}</div>
-                <div className='cart-item-actions'>
-                    {/* buttons */}
-                    <img 
-                    alt='increase_the_quantity'
-                    src='https://cdn-icons-png.flaticon.com/512/1237/1237946.png'
-                    className='action-icons'
-                    onClick={this.increaseQuantity}/>
-                    <img 
-                    alt='decrease_the_quantity' 
-                    src='https://cdn-icons-png.flaticon.com/512/56/56889.png' 
-                    className='action-icons'/>
-                    <img 
-                    alt='delete_the_item' 
-                    src='https://cdn-icons-png.flaticon.com/512/542/542724.png' 
-                    className='action-icons'/>
-                </div>
-                </div>
+    render () {
+      console.log('this.props', this.props);
+      const { price, title, qty } = this.props.product;
+      return (
+        <div className="cart-item">
+          {this.props.jsx}
+          <div className="left-block">
+            <img style={styles.image} />
+          </div>
+          <div className="right-block">
+            <div style={ { fontSize: 25 } }>{title}</div>
+            <div style={ { color: '#777' } }>Rs {price} </div>
+            <div style={ { color: '#777' } }>Qty: {qty} </div>
+            <div className="cart-item-actions">
+              {/* Buttons */}
+              <img
+                alt="increase"
+                className="action-icons"
+                src="https://cdn-icons-png.flaticon.com/128/4315/4315609.png"
+                onClick={this.increaseQuantity}
+              />
+              <img
+                alt="decrease"
+                className="action-icons"
+                src="https://cdn-icons-png.flaticon.com/512/4436/4436695.png"
+                onClick={this.decreaseQuantity}
+              />
+              <img
+                alt="delete"
+                className="action-icons"
+                src="https://cdn-icons-png.flaticon.com/512/5028/5028066.png"
+              />
             </div>
-        );
-
+          </div>
+        </div>
+      );
     }
-
-}
-const styles={
-    image:{
-    height:110,
-    width:110,
-    borderRadius:4,
-    background:'#ccc'
-
+  }
+  
+  const styles = {
+    image: {
+      height: 110,
+      width: 110,
+      borderRadius: 4,
+      background: '#ccc'
     }
-}
-export default CartItem;
+  }
+  
+  export default CartItem;
